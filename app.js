e95670e9e7fcb86fc8ae13dba3cdc8abea77223a
Elvan H <elvanhd@gmail.com>
2021-12-08 19:58:00 +0700

    binding a method to an event

--- app.js
+++ app.js
@@ -3,6 +3,15 @@ const app = Vue.createApp({
     return {
       counter: 0
     };
+  },
+
+  methods: {
+    increment() {
+      this.counter++;
+    },
+    decrement() {
+      this.counter--;
+    }
   }
 });
 
